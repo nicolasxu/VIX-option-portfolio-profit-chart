62ef78e913cc5b19ad013536a10837a9639acc62
Shenxin(Nicolas) Xu <xu.shenxin@gmail.com>
2014-12-12 17:15:33 +0800

    update

--- clickEdit.js
+++ clickEdit.js
@@ -3,13 +3,36 @@ angular.module('optionChart')
 		return {
 			restrict: 'A',
 			link: function postLink(scope, iElement, iAttrs) {
+				
 				console.info('clickEdit is running');
 
+				scope.tempValue = '';
+				var templates = {
+					text: '<input type="text" ng-model="tempValue">',
+					select:'<select ng-model="tempValue" ng-options="name for name in iAttrs.selectData"></select>'
+				};
+
+				var insertBoxHtml = '<div class="insert-box">' +
+										'<h1>This is a box</h1>' +
+									'</div>';
+
 				iElement.on('click', function($event){
+					$event.stopPropagation();
 					console.info('clicked');
 
+					if($('.insert-box').length === 0) {
+						iElement.append(insertBoxHtml);
+					} else 
+					{
+						$('.insert-box').remove();
+					}
+					
+					$('body').click(function(){
+						$('.insert-box').remove();
+					});
+
 				});
-				console.log(iElement);
+
 			}
 		};
 	}]);
